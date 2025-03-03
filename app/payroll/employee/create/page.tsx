ac884fcc44ca02414efe7af6baf645b79a818bab
lester130608 <lrojas@dttcoaching.com>
2025-03-03 09:35:55 -0500

    Eliminar payrollService.ts para corregir error de duplicación

--- app/payroll/employee/create/page.tsx
+++ app/payroll/employee/create/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
-import { getEmployeesForSupervisor } from "@/lib/payrollService";
+import { getEmployeesForSupervisor } from "../../../../lib/payrollService";
 import PayrollFormEmployee, { Employee, PayrollEntry } from "@/components/PayrollFormEmployee";
 
 export default function CreateEmployeePayrollPage() {
